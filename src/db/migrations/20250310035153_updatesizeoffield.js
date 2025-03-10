be7c038e89b278e4cf97855d830d216a80b3f461
manish2-0 <manishkumawat12955@gmail.com>
2025-03-10 09:49:54 +0530

    changes

---
+++ src/db/migrations/20250310035153_updatesizeoffield.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+
+    return knex.schema.alterTable('user', function(table) {
+        table.text('user_first_name').alter(); // Change column type to TEXT
+      });
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
