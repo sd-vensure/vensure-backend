7e04adb5a0d11a2baa25576902850ee156c9087d
manish2-0 <manishkumawat12955@gmail.com>
2025-03-15 10:14:01 +0530

    chanhes

--- knexfile.js
+++ knexfile.js
@@ -10,10 +10,42 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE
+      host: process.env.MYSQL_HOST_MAIN,
+      user: process.env.MYSQL_USER_MAIN,
+      password: process.env.MYSQL_PASSWORD_MAIN,
+      database: process.env.MYSQL_DATABASE_MAIN
+    },
+    migrations: {
+      directory: './src/db/migrations',
+    },
+    seeds: {
+      directory: './src/db/seeds',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+      afterCreate: (conn, done) => {
+        conn.on('error', (err) => {
+          if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was lost. Reconnecting...');
+          } else {
+            console.error('Database connection error:', err);
+          }
+        });
+        done(null, conn);
+      },
+    },
+    acquireConnectionTimeout: 10000
+
+  },
+  
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.MYSQL_HOST_MAIN,
+      user: process.env.MYSQL_USER_MAIN,
+      password: process.env.MYSQL_PASSWORD_MAIN,
+      database: process.env.MYSQL_DATABASE_MAIN
     },
     migrations: {
       directory: './src/db/migrations',
